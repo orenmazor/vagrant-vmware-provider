643946305460203b17ba741cc498417f516e7366
Oren Mazor <oren.mazor@gmail.com>
2014-09-08 14:30:02 -0400

    start instance

---
+++ lib/vagrant-vmware-provider/action/run_instance.rb
@@ -0,0 +1,41 @@
+require "log4r"
+
+module VagrantPlugins
+  module VMwareProvider
+    module Action
+      class RunInstance
+        def initialize(app, env)
+          @app    = app
+          @logger = Log4r::Logger.new("vagrant_vmware_provider::action::run_instance")
+        end
+
+        def call(env)
+          config = env[:machine].provider_config
+          
+          dump_config(env, config)
+
+          # Terminate the instance if we were interrupted
+          terminate(env) if env[:interrupted]
+          @app.call(env)
+        end
+
+        private 
+
+        def dump_config(env, config)
+          config.each_pairs do |k,v|
+            env[:ui].info("#{k} -- #{v}")
+          end
+        end
+
+        def terminate(env)
+          destroy_env = env.dup
+          destroy_env.delete(:interrupted)
+          destroy_env[:config_validate] = false
+          destroy_env[:force_confirm_destroy] = true
+          env[:action_runner].run(Action.action_destroy, destroy_env)
+        end
+
+      end
+    end
+  end
+end
