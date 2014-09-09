236b8fc9bf493d67ddc6cdbfa6baaaa4eb9fa545
Oren Mazor <oren.mazor@gmail.com>
2014-09-09 14:44:46 -0400

    :rocket:

--- lib/vagrant-vmware-provider/action.rb
+++ lib/vagrant-vmware-provider/action.rb
@@ -47,7 +47,6 @@ module VagrantPlugins
 
       # This action is called when `vagrant provision` is called.
       def self.action_provision
-        raise "not yet implemented"
         Vagrant::Action::Builder.new.tap do |b|
           b.use ConfigValidate
           b.use Call, IsCreated do |env, b2|
@@ -57,7 +56,6 @@ module VagrantPlugins
             end
 
             b2.use Provision
-            b2.use SyncFolders
           end
         end
       end
@@ -99,23 +97,13 @@ module VagrantPlugins
 
       def self.action_ssh_run
         raise "not yet implemented"
-        Vagrant::Action::Builder.new.tap do |b|
-          b.use ConfigValidate
-          b.use Call, IsCreated do |env, b2|
-            if !env[:result]
-              not_created!
-              b2.use MessageNotCreated
-              next
-            end
-
-            b2.use SSHRun
-          end
-        end
       end
 
       def self.action_prepare_boot
         Vagrant::Action::Builder.new.tap do |b|
           b.use Provision
+          b.use SyncedFolders
+          # b.use SyncedFolderCleanup 
         end
       end
 
@@ -145,46 +133,17 @@ module VagrantPlugins
       end
 
       def self.action_reload
-        raise "not yet implemented"
-        Vagrant::Action::Builder.new.tap do |b|
-          b.use ConfigValidate
-          b.use ConnectAWS
-          b.use Call, IsCreated do |env, b2|
-            if !env[:result]
-              b2.use MessageNotCreated
-              next
-            end
-
-            b2.use action_halt
-            b2.use Call, WaitForState, :stopped, 120 do |env2, b3|
-              if env2[:result]
-                b3.use action_up
-              else
-                # TODO we couldn't reach :stopped, what now?
-              end
-            end
-          end
-        end
+        env[:ui].info "not implemented. do vagrant halt; vagrant up manually"
       end
 
       action_root = Pathname.new(File.expand_path("../action", __FILE__))
       autoload :IsCreated, action_root.join("is_created")
       autoload :IsStopped, action_root.join("is_stopped")
-      # autoload :MessageAlreadyCreated, action_root.join("message_already_created")
       autoload :MessageNotCreated, action_root.join("message_not_created")
-      # autoload :MessageWillNotDestroy, action_root.join("message_will_not_destroy")
       autoload :ReadSSHInfo, action_root.join("read_ssh_info")
       autoload :ReadState, action_root.join("read_state")
       autoload :RunInstance, action_root.join("run_instance")
-      # autoload :StartInstance, action_root.join("start_instance")
       autoload :StopInstance, action_root.join("stop_instance")
-      # autoload :SyncFolders, action_root.join("sync_folders")
-      # autoload :TerminateInstance, action_root.join("terminate_instance")
-      # autoload :TimedProvision, action_root.join("timed_provision") # some plugins now expect this action to exist
-      # autoload :WaitForState, action_root.join("wait_for_state")
-      # autoload :WarnNetworks, action_root.join("warn_networks")
-      # autoload :ElbRegisterInstance, action_root.join("elb_register_instance")
-      # autoload :ElbDeregisterInstance, action_root.join("elb_deregister_instance")
     end
   end
 end
